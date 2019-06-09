8e5f4c2d46e8308f3fa78fa34dcea09cf0e3400b
Leon-Cao <skyue1@hotmail.com>
2019-06-09 14:05:40 +0800

    Only support PID alogrithm. First time pass one loop running.

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,13 +12,26 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+
+  /**
+   * PID Coefficients
+   */ 
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
 
 }
 
@@ -26,5 +39,7 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  double vTotalError;
+  vTotalError = - Kp * p_error - Ki * i_error - Kd * d_error;
+  return vTotalError;  // TODO: Add your total error calc here!
 }
